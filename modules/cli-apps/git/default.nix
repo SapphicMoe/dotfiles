6faaed2a954a0d0af17ded1216592b6f6d72010c
Chloe <solely@riseup.net>
2024-03-30 22:32:30 +0500

    feat: add git commit signing signatures

--- modules/cli-apps/git/default.nix
+++ modules/cli-apps/git/default.nix
@@ -1,15 +1,25 @@
 { pkgs, ... }:
 
-{
+let
+  signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJug+9rnFngnFQpY0lAO0NuVBhDCcJc5imPHazgOSTTx";
+in {
+  home.file.".ssh/allowed_signers".text = "
+    * ${signingKey}
+  ";
   programs.git = {
     enable = true;
     userName = "Chloe";
     userEmail = "solely@riseup.net";
     
     extraConfig = {
-      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJug+9rnFngnFQpY0lAO0NuVBhDCcJc5imPHazgOSTTx";
-      gpg.format = "ssh";
-      gpg.ssh.program = "${pkgs._1password-gui}/bin/op-ssh-sign";
+      user.signingkey = signingKey;
+      gpg = {
+        format = "ssh";
+        ssh = {
+          program = "${pkgs._1password-gui}/bin/op-ssh-sign";
+          allowedSignersFile = "~/.ssh/allowed_signers";
+        };
+      };
       commit.gpgsign = true;
     };
   };
