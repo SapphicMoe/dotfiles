fd5d3ca7461b4467c4188cc14e3547be686ef890
Chloe <chloe@sapphic.moe>
2024-12-07 02:29:05 +0500

    chore: add catppuccin flake :)

--- flake.nix
+++ flake.nix
@@ -2,16 +2,26 @@
   description = "NixOS configuration for the Sapphic Angels system.";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs = {
+      url = "nixpkgs/nixos-unstable";
+    };
 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # Catppuccin theme
+    catppuccin = {
+      url = "github:catppuccin/nix";
+    };
+
     # Nix Language Server
-    nil.url = "github:oxalica/nil";
-    
+    nil = {
+      url = "github:oxalica/nil";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     # nix-ld-rs
     nix-ld-rs = {
       url = "github:nix-community/nix-ld-rs";
@@ -19,12 +29,16 @@
     };
 
     # NixOS on WSL
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ { 
     self,
     nixpkgs,
+    catppuccin,
     home-manager, 
     nixos-wsl,
     ...
@@ -53,6 +67,7 @@
           pkgs = pkgsFor.x86_64-linux;
           specialArgs = { inherit inputs outputs; };
           modules = [
+            catppuccin.nixosModules.catppuccin
             ./hosts/sapphic/configuration.nix
           ];
         };
@@ -62,6 +77,7 @@
           specialArgs = { inherit inputs outputs; };
           modules = [
             nixos-wsl.nixosModules.default
+            catppuccin.homeManagerModules.catppuccin
             ./hosts/solstice/configuration.nix
           ];
         };
@@ -72,6 +88,7 @@
         "chloe@sapphic" = inputs.home-manager.lib.homeManagerConfiguration {
           pkgs = pkgsFor.x86_64-linux;
           modules = [
+            catppuccin.homeManagerModules.catppuccin
             ./home/chloe/sapphic.nix
           ];
           extraSpecialArgs = { inherit inputs outputs; };
@@ -80,6 +97,7 @@
         "chloe@solstice" = inputs.home-manager.lib.homeManagerConfiguration {
           pkgs = pkgsFor.x86_64-linux;
           modules = [
+            catppuccin.nixosModules.catppuccin
             ./home/chloe/solstice.nix
           ];
           extraSpecialArgs = { inherit inputs outputs; };
