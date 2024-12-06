8bbf7fbd9a1e161e595ab2981de3e45beeea2559
Chloe <chloe@sapphic.moe>
2024-12-07 02:32:57 +0500

    chore: update cider

--- packages/cider.nix
+++ packages/cider.nix
@@ -6,26 +6,22 @@
 
 appimageTools.wrapType2 rec {
   pname = "cider";
-  version = "2.3.2";
-  name = "${pname}-${version}";
+  version = "2.5.9";
 
   src = /home/chloe/Downloads/Cider-linux-appimage-x64.AppImage;
 
   extraInstallCommands = let
-    contents = appimageTools.extract { inherit name src; };
+    contents = appimageTools.extract { inherit pname version src; };
   in ''
-    mv $out/bin/${name} $out/bin/${pname}
-
     install -m 444 -D ${contents}/${pname}.desktop -t $out/share/applications
     substituteInPlace $out/share/applications/${pname}.desktop \
       --replace 'Exec=AppRun' 'Exec=${pname}'
     cp -r ${contents}/usr/share/icons $out/share
   '';
 
-  meta = with lib; {
+  meta = {
     description = "A new look into listening and enjoying Apple Music in style and performance.";
-    homepage = "https://github.com/ciderapp/Cider";
-    license = licenses.gpl3;
+    homepage = "https://cidercollective.itch.io/cider";
     platforms = [ "x86_64-linux" ];
   };
 }
