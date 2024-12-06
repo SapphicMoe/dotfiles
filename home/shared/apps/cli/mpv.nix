d9535cf73b105280eb5c4964412067ce29794b4f
Chloe <chloe@sapphic.moe>
2024-12-07 02:32:33 +0500

    chore: enable catppucin theme

--- home/shared/apps/cli/mpv.nix
+++ home/shared/apps/cli/mpv.nix
@@ -1,5 +1,6 @@
 {
   programs.mpv = {
     enable = true;
+    catppuccin.enable = true;
   };
 }
--- home/shared/files.nix
+++ home/shared/files.nix
@@ -9,24 +9,14 @@
       vault = "Development"
     '';
 
-    # Catppuccin theme for btop
-    ".config/btop/themes/catppuccin_mocha.theme" = {
+    # Catppuccin theme for Konsole
+    ".local/share/konsole/catppuccin-mocha.colorscheme" = {
       source = pkgs.fetchFromGitHub {
         owner = "catppuccin";
-        repo = "btop";
-        rev = "ecb8562";
-        sha256 = "sha256-ovVtupO5jWUw6cwA3xEzRe1juUB8ykfarMRVTglx3mk=";
-      } + "/catppuccin_mocha.theme";
-    };
-
-    # Catppuccin theme for Tilix
-    ".config/tilix/schemes/Catppuccin-Mocha.json" = {
-      source = pkgs.fetchFromGitHub {
-        owner = "catppuccin";
-        repo = "tilix";
-        rev = "3fd05e03419321f2f2a6aad6da733b28be1765ef";
-        hash = "sha256-SI7QxQ+WBHzeuXbTye+s8pi4tDVZOV4Aa33mRYO276k=";
-      } + "/src/Catppuccin-Mocha.json";
+        repo = "konsole";
+        rev = "7d86b8a1e56e58f6b5649cdaac543a573ac194ca";
+        sha256 = "sha256-EwSJMTxnaj2UlNJm1t6znnatfzgm1awIQQUF3VPfCTM=";
+      } + "/Catppuccin-Mocha.colorscheme";
     };
   };
 }
