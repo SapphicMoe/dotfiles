970ba49c38e56d42f05d82094107412fa4ae48d4
Chloe <chloe@sapphic.moe>
2024-12-07 05:07:49 +0500

    chore: fix deprecated errors

--- modules/system/graphics.nix
+++ modules/system/graphics.nix
@@ -1,10 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+    enable32Bit = true;
   };
 
   # Load NVIDIA driver for X11 and Wayland
