89829d502865a19ae546dc36da2bf9dafaad8817
Chloe <chloe@sapphic.moe>
2024-12-22 18:12:27 +0500

    chore: reshuffle some settings around

--- hosts/eris/configuration.nix
+++ hosts/eris/configuration.nix
@@ -1,42 +1,56 @@
-{ pkgs, inputs, ... }:
+{ pkgs, ... }:
 
 {
   imports = [
     ./hardware-configuration.nix
     ../shared
 
+    ../../modules/system/acpi.nix
     ../../modules/system/audio.nix
     ../../modules/system/bootloader.nix
+    ../../modules/system/fingerprint
     ../../modules/system/kde.nix
 
     ../../modules/apps/gui/1password.nix
     ../../modules/apps/gui/steam.nix
   ];
 
-  environment.systemPackages = with pkgs; [
-    catppuccin-sddm-corners
-    catppuccin-cursors.mochaPink
-  ];
-  
-  services.flatpak.enable = true;
-  services.tailscale.enable = true;
-  networking.hostName = "eris";
-  networking.networkmanager.enable = true;
+  networking = {
+    hostName = "eris";
+    networkmanager.enable = true;
+  };
 
-  # Use this when using the fingerprint sensor.
-  services.fprintd = {
-    enable = true;
-    tod = {
+  boot = {
+    blacklistedKernelModules = ["i915"];
+    kernelParams = [
+      "snd_hda_intel.power_save=1"
+      "i915.enable_psr=0"
+      "bbswitch.load_state=0"
+      "bbswitch.unload_state=1"
+    ];
+  };
+
+  hardware = {
+    graphics.enable = true;
+    graphics.extraPackages = with pkgs; [ intel-media-driver ];
+    bluetooth.enable = true;
+    enableRedistributableFirmware = true;
+    cpu.intel.updateMicrocode = true;
+    trackpoint = {
       enable = true;
-      driver = inputs.nixos-06cb-009a-fingerprint-sensor.lib.libfprint-2-tod1-vfs0090-bingch {
-        calib-data-file = ./fingerprint/calib-data.bin;
-      };
+      emulateWheel = true;
     };
   };
-  
-  # Use this when enrolling new fingerprints.
-  # services.open-fprintd.enable = true;
-  # services.python-validity.enable = true;
+
+  services = {
+    fstrim.enable = true;
+    fwupd.enable = true;
+    power-profiles-daemon.enable = false;
+    tlp.enable = true;
+    throttled.enable = true;
+  };
+
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
 
   system.stateVersion = "23.11"; # Initial NixOS version
 }
