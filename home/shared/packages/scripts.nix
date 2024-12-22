65cad84a750014ea2a42c6c6c8d3f826274af99d
Chloe <chloe@sapphic.moe>
2024-12-22 18:10:51 +0500

    feat: venv command

--- home/shared/packages/scripts.nix
+++ home/shared/packages/scripts.nix
@@ -5,11 +5,16 @@
     (writeShellScriptBin "shash" ''
       nix hash to-sri --type sha256 $(nix-prefetch-url ''$1)
     '')
+    
     (writeShellScriptBin "nix-rb" ''
       sudo nixos-rebuild switch --flake .#$1
     '')
     (writeShellScriptBin "hm-rb" ''
       nix run home-manager -- switch --flake .#$1
+    '')
+
+    (writeShellScriptBin "create-venv" ''
+      nix-shell -p python3 --command "python -m venv .venv --copies"
     '')
   ];
 }
